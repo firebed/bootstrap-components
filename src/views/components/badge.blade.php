f6c739f8fe780ad866f8c3d127fac39d432097bf
okangr <okan.giritli@gmail.com>
2021-03-31 18:24:28 +0300

    Add types to badges

--- src/views/components/badge.blade.php
+++ src/views/components/badge.blade.php
@@ -1,1 +1,18 @@
-<span {{ $attributes->merge(['class' => 'badge rounded-pill']) }} {{ $attributes }}>{{ $slot }}</span>
+@props([
+    'type' => NULL
+])
+
+@php
+    $type = $type ?? "primary";
+
+    $color = match($type){
+        'primary'   => 'bg-blue-200',
+        'secondary' => 'bg-gray-200',
+        'success'   => 'bg-green-200',
+        'info'      => 'bg-teal-200',
+        'warning'   => 'bg-yellow-200',
+        'danger'    => 'bg-red-200',
+    };
+@endphp
+
+<span {{ $attributes->merge(['class' => "badge $color rounded-pill"]) }} {{ $attributes }}>{{ $slot }}</span>
