a067c859968c73a5ea536186594f45d5540bf525
okangr <okan.giritli@gmail.com>
2021-03-31 17:02:54 +0300

    Made model compatible for livewire and non livewire components

--- src/views/components/modal.blade.php
+++ src/views/components/modal.blade.php
@@ -1,23 +1,32 @@
 @props([
-'size' => null,
-'backdrop' => 'true'
+    'size' => null,
+    'backdrop' => 'true'
 ])
 
-<div
-        wire:ignore.self
-        x-data="{ show: @entangle($attributes->wire('model')) }"
-        x-init="
-        new bootstrap.Modal($el, {backdrop: '{{ $backdrop }}'});
-        $watch('show', value => value ? bootstrap.Modal.getInstance($el).show() : bootstrap.Modal.getInstance($el).hide());
-        $el.addEventListener('hidden.bs.modal', () => show = false);
-    "
-        id="{{ $id ?? md5($attributes->wire('model')) }}"
-        class="modal fade"
-        tabindex="-1" xmlns:wire="http://www.w3.org/1999/xhtml">
-
-    <div class="modal-dialog shadow @isset($size) modal-{{ $size }} @endisset">
-        <div class="modal-content">
-            {{ $slot }}
+@if($attributes->wire('model'))
+    <div wire:ignore.self
+         x-data="{ show: @entangle($attributes->wire('model')) }"
+         x-init="
+            new bootstrap.Modal($el, {backdrop: '{{ $backdrop }}'});
+            $watch('show', value => value ? bootstrap.Modal.getInstance($el).show() : bootstrap.Modal.getInstance($el).hide());
+            $el.addEventListener('hidden.bs.modal', () => show = false);
+        "
+         id="{{ $id ?? md5($attributes->wire('model')) }}"
+         class="modal fade"
+         tabindex="-1"
+    >
+        <div class="modal-dialog shadow @isset($size) modal-{{ $size }} @endisset">
+            <div class="modal-content">
+                {{ $slot }}
+            </div>
         </div>
     </div>
-</div>
+@else
+    <div id="{{ $id ?? substr(md5(mt_rand()), 0, 7) }}" class="modal fade" tabindex="-1">
+        <div class="modal-dialog shadow @isset($size) modal-{{ $size }} @endisset">
+            <div class="modal-content">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+@endif
