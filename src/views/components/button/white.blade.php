a1b4b90d60761ba923042229e1cfc3cbd04db1bd
okangr <okan.giritli@gmail.com>
2021-04-01 11:26:24 +0300

    Small fixes

--- src/views/components/button/white.blade.php
+++ src/views/components/button/white.blade.php
@@ -2,6 +2,6 @@
     'outline' => false,
 ])
 
-<x-bs::button {{ $attributes->merge(['class' => ($outline ? 'btn-outline-white' : 'btn-white') . ($attributes->has('disabled') ? ' bg-gray-100' : '')]) }}>
+<x-bs::button {{ $attributes->merge(['class' => ($outline ? 'btn-outline-white' : 'btn-white')]) }}>
     {{{ $slot }}}
 </x-bs::button>
