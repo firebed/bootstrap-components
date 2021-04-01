4f8dce6921339dec630cd6f28889d95311633104
okangr <okan.giritli@gmail.com>
2021-04-01 11:09:49 +0300

    Added light background color when disabled

--- src/views/components/button/white.blade.php
+++ src/views/components/button/white.blade.php
@@ -2,6 +2,6 @@
     'outline' => false,
 ])
 
-<x-bs::button {{ $attributes->merge(['class' => $outline ? 'btn-outline-white' : 'btn-white']) }}>
+<x-bs::button {{ $attributes->merge(['class' => ($outline ? 'btn-outline-white' : 'btn-white') . ($attributes->has('disabled') ? 'bg-gray-100' : '')]) }}>
     {{{ $slot }}}
 </x-bs::button>
