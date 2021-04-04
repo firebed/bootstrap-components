398e4de00bc6e22771bc739a11660fb443bbbcf8
okangr <okan.giritli@gmail.com>
2021-04-04 23:50:31 +0300

    Removed shadow from button link

--- src/views/components/button/link.blade.php
+++ src/views/components/button/link.blade.php
@@ -3,7 +3,7 @@
 <button
         {{ $attributes->merge([
             'type' => 'button',
-            'class' => 'btn btn-link text-decoration-none' . ($size ? " btn-$size" : ''),
+            'class' => 'btn btn-link shadow-none text-decoration-none' . ($size ? " btn-$size" : ''),
         ]) }}
 >
     {{ $slot }}
