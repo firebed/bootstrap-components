646619c3a9a033447d17c9731f308c8d7341c80a
okangr <okan.giritli@gmail.com>
2021-04-05 22:24:55 +0300

    Added rich text editor

--- src/views/components/input/rich-text.blade.php
+++ src/views/components/input/rich-text.blade.php
@@ -1,27 +1,29 @@
 @props([
-    'toolbar' => 'fontsizeselect | bold italic underline | forecolor | alignleft aligncenter alignright alignjustify | outdent indent'
+    'menubar' => '',
+    'toolbar' => 'fontsizeselect | bold italic underline | forecolor | alignleft aligncenter alignright alignjustify | outdent indent',
+    'error'   => null
 ])
 
 <div
-        x-data="{ value: @entangle($attributes->wire('model')) }"
-        x-init="
-            tinymce.init({
-                target: $refs.input,
-                menubar: '',
-                toolbar: '{{ $toolbar }}',
-                setup: function (editor) {
-                    editor.on('input', function (e) {
-                        value = editor.getContent();
-                    });
-                }
+    x-data="{ value: @entangle($attributes->wire('model')) }"
+    x-init="tinymce.init({
+        target: $refs.input,
+        menubar: '{{ $menubar }}',
+        toolbar: '{{ $toolbar }}',
+        setup: function (editor) {
+            editor.on('input', function (e) {
+                value = editor.getContent();
             });
-            $watch('value', v => console.log(v));
-        "
-        wire:ignore
+            $watch('value', v => !editor.hasFocus() && editor.setContent(v));
+        }
+    });"
+    wire:ignore
 >
-        <textarea x-ref="input" {{ $attributes->whereDoesntStartWith('wire:model')->class('form-control opacity-0') }}></textarea>
+    <textarea x-ref="input" {{ $attributes->whereDoesntStartWith('wire:model')->class('form-control opacity-0') }}></textarea>
 </div>
 
-@error($attributes->wire('model')->value)
-<div class="text-danger small mt-1">{{ $message }}</div>
-@enderror
+@if($error)
+    @error($error)
+    <div class="text-danger small mt-1">{{ $message }}</div>
+    @enderror
+@endif
