f5c27a76de6dcf975ac60c666d830b77eef331fd
okangr <okan.giritli@gmail.com>
2021-04-06 03:49:25 +0300

    Slimselect bug fixes

--- src/views/components/input/slim-select.blade.php
+++ src/views/components/input/slim-select.blade.php
@@ -4,14 +4,25 @@
 ])
 
 <div wire:ignore
-     x-data
+     x-data="{ values: @entangle($attributes->wire('model')) }"
      x-init="new SlimSelect({
         select: $refs.select,
         showSearch: false,
         allowDeselect: {{ $allowDeselect ? 'true' : 'false' }},
-     })"
+        onChange: () => {
+            const selected = $refs.select.slim.selected()
+            for(let i=0; i<selected.length; i++) {
+                if (selected[i] !== values[i]) {
+                    values = $refs.select.slim.selected()
+                    break
+                }
+            }
+        }
+     })
+     $refs.select.slim.set(values);
+    "
 >
-    <select hidden x-ref="select" {{ $attributes }}>
+    <select hidden x-ref="select" {{ $attributes->whereDoesntStartWith('wire:model') }}>
         {{ $slot }}
     </select>
 </div>
