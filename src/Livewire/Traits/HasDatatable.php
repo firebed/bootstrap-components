4ce6f7f782305d11103b66736f5f0a89a4d36c9d
okangr <okan.giritli@gmail.com>
2021-04-14 18:32:12 +0300

    Added sorting options

--- src/Livewire/Traits/HasDatatable.php
+++ src/Livewire/Traits/HasDatatable.php
@@ -11,11 +11,25 @@ trait HasDatatable
     public array $selected  = [];
     public bool  $selectAll = false;
 
+    public string $sortField     = '';
+    public string $sortDirection = '';
+
     public function updatedSelectAll($selectAll): void
     {
         $this->selected = $selectAll
             ? $this->getModels()->pluck('id')->map(fn($id) => (string)$id)
             : [];
+    }
+
+    public function sortBy($field): void
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortDirection = 'asc';
+        }
+
+        $this->sortField = $field;
     }
 
     protected function doesntHaveSelections(): bool
