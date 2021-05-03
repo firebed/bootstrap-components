0fa975697931814d834cfa0ef1de1d309e507040
okangr <okan.giritli@gmail.com>
2021-05-03 22:33:02 +0300

    Checkbox label class forwarding

--- src/views/components/input/checkbox.blade.php
+++ src/views/components/input/checkbox.blade.php
@@ -1,8 +1,9 @@
 @props([
-    'error' => NULL
+    'error' => NULL,
+    'lblClass' => NULL
 ])
 
 <div class="form-check mb-0">
     <input type="checkbox" {{ $attributes->class(['form-check-input', 'is-invalid' => $error && $errors->has($error)]) }} autocomplete="off">
-    <label for="{{ $attributes->get('id') }}" class="form-check-label">{{ $slot }}</label>
+    <label for="{{ $attributes->get('id') }}" class="form-check-label@if($lblClass) {{ $lblClass }} @endif">{{ $slot }}</label>
 </div>
