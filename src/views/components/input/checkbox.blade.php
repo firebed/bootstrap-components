ed6a753b098db554a2138c877b41fb9ab3b1c400
okangr <okan.giritli@gmail.com>
2021-05-03 22:35:17 +0300

    Typo

--- src/views/components/input/checkbox.blade.php
+++ src/views/components/input/checkbox.blade.php
@@ -5,5 +5,5 @@
 
 <div class="form-check mb-0">
     <input type="checkbox" {{ $attributes->class(['form-check-input', 'is-invalid' => $error && $errors->has($error)]) }} autocomplete="off">
-    <label for="{{ $attributes->get('id') }}" class="form-check-label@if($lblClass) {{ $lblClass }} @endif">{{ $slot }}</label>
+    <label for="{{ $attributes->get('id') }}" class="form-check-label @if($lblClass) {{ $lblClass }} @endif">{{ $slot }}</label>
 </div>
