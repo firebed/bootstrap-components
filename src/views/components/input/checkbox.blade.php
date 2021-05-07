6ea46eeaa68b9b98eabb049dbb829fc06a39f0b6
okangr <okan.giritli@gmail.com>
2021-05-07 19:38:40 +0300

    Added error messages for checkbox/radio components

--- src/views/components/input/checkbox.blade.php
+++ src/views/components/input/checkbox.blade.php
@@ -6,4 +6,10 @@
 <div class="form-check mb-0">
     <input type="checkbox" {{ $attributes->class(['form-check-input', 'is-invalid' => $error && $errors->has($error)]) }} autocomplete="off">
     <label for="{{ $attributes->get('id') }}" class="form-check-label @if($lblClass) {{ $lblClass }} @endif">{{ $slot }}</label>
+
+    @if($error)
+        @error($error)
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    @endif
 </div>
