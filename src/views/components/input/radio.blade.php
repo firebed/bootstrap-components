90d43d604186b511f236fc731275efd71677d6c5
okangr <okan.giritli@gmail.com>
2021-05-08 02:23:24 +0300

    Fixed label class form radio/checkbox

--- src/views/components/input/radio.blade.php
+++ src/views/components/input/radio.blade.php
@@ -1,10 +1,11 @@
 @props([
-    'error' => NULL
+    'error' => NULL,
+    'labelClass' => '',
 ])
 
 <div class="form-check mb-0">
     <input type="radio" {{ $attributes->class(['form-check-input', 'is-invalid' => $error && $errors->has($error)]) }} autocomplete="off">
-    <label for="{{ $attributes->get('id') }}" class="form-check-label">{{ $slot }}</label>
+    <label for="{{ $attributes->get('id') }}" class="form-check-label {{ $labelClass }}">{{ $slot }}</label>
 
     @if($error)
         @error($error)
