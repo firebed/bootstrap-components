552a5b1c6b9553c5ca97f56e9687927cec8feee1
okangr <okan.giritli@gmail.com>
2021-05-12 01:36:30 +0300

    Variable name fix

--- src/views/components/input/checkbox.blade.php
+++ src/views/components/input/checkbox.blade.php
@@ -5,7 +5,7 @@
 
 <div class="form-check mb-0">
     <input type="checkbox" {{ $attributes->class(['form-check-input', 'is-invalid' => $error && $errors->has($error)]) }} autocomplete="off">
-    <label for="{{ $attributes->get('id') }}" class="form-check-label {{ $lblClass }}">{{ $slot }}</label>
+    <label for="{{ $attributes->get('id') }}" class="form-check-label {{ $labelClass }}">{{ $slot }}</label>
 
     @if($error)
         @error($error)
